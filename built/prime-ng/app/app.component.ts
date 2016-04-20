92d0bd0e82a2a6ce1627d8d525e9fe85da45e7f8
simon <simon.c.eastwood@gmail.com>
2016-04-20 11:17:34 +0000

    no comment

--- built/prime-ng/app/app.component.ts
+++ built/prime-ng/app/app.component.ts
@@ -4,7 +4,7 @@ import {InputText} from 'primeng/primeng';
 @Component({
 	selector: 'my-app',
 	template: `
-        <h1>My First 2 App</h1>
+        <p>This is a live Angular 2 component</p>
         <input type="text" pInputText [(ngModel)]="text" />
         {{text}}
     `,
