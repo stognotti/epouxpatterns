0f9e83fa6c8dbd8f6314bf0f80d3220542ece6b3
simon <simon.c.eastwood@gmail.com>
2016-04-21 09:51:21 +0000

    no comment

--- site/built/prime-ng/preview-code/boot.ts
+++ site/built/prime-ng/preview-code/boot.ts
@@ -1,8 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
-import {ButtonPreview1Component} from './buttonpreview1component';
 
-bootstrap(AppComponent);
+import {ButtonPreview1Component} from './buttonpreview1component';
 
 
 import {ButtonPreview1Component} from './buttonpreview1component';
