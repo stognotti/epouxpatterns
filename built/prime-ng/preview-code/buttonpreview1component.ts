5988a64df5ac1c8e9e6da0c2f5f4842222b29123
simon <simon.c.eastwood@gmail.com>
2016-04-21 13:30:06 +0000

    no comment

--- built/prime-ng/preview-code/buttonpreview1component.ts
+++ built/prime-ng/preview-code/buttonpreview1component.ts
@@ -5,13 +5,13 @@ import {Button} from 'primeng/primeng';
 @Component({
 	selector: 'button-preview-1-component',
 	template: `
-	<div style="font-family: Awesome; font-size: 25px; colour: #FFFFFF; background-color: #3183BC; stroke: #CCCCCC">
-        <button pButton type="button" label="Click-3" ></button>
+	<div style="font-family: Awesome; font-size: 25px; colour: #FFFFFF;  stroke: #CCCCCC">
+        <button pButton type="button" label="Submit" style="background-color: #3183BC;" ></button>
     </div>
     `,
     styles: [`
     .ui-button-text {
-      font-size: 2px;
+      
     }
     `],
     directives: [Button]
