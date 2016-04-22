bf3f1231ed0ce4246b08efa3337b3dbc09c2ef11
simon <simon.c.eastwood@gmail.com>
2016-04-22 14:56:55 +0000

    no comment

--- site/built/prime-ng/preview-code/buttonpreview1component/buttonpreview1component.ts
+++ site/built/prime-ng/preview-code/buttonpreview1component/buttonpreview1component.ts
@@ -4,8 +4,8 @@ import {Button} from 'primeng/primeng';
 
 @Component({
 	selector: 'button-preview-1-component',
-  styleUrls: ['buttonpreview1component/buttonpreview1component.css'],
-	templateUrl: 'buttonpreview1component/buttonpreview1component.html',
+  styleUrls: ['preview-code/buttonpreview1component/buttonpreview1component.css'],
+	templateUrl: 'preview-code/buttonpreview1component/buttonpreview1component.html',
     directives: [Button]
 })
 export class ButtonPreview1Component {
